a0423a7b1b1518a0e1da0e9367f26eb4b42744d4
JunseongKim0105 <juns.kim0105@gmail.com>
2023-02-11 20:44:45 -0500

    make submit router

--- server.js
+++ server.js
@@ -26,11 +26,13 @@ app.use(
 );
 app.use(express.static("public"));
 
+const { pool } = require("pg");
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require("./routes/users-api");
 const widgetApiRoutes = require("./routes/widgets-api");
 const usersRoutes = require("./routes/users");
+// const registration = require("./routes/registration");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -40,12 +42,55 @@ app.use("/api/widgets", widgetApiRoutes);
 app.use("/users", usersRoutes);
 // Note: mount other resources here, using the same pattern above
 
+// const pool = new Pool({
+//   user: "labber",
+//   password: "labber",
+//   host: "localhost",
+//   database: "midterm",
+// });
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
-  res.render("index1");
+  // const resources = require("./data/resources.json");
+  // const comments = require("./data/comments.json");
+
+  res.render(
+    "index"
+    // resources,
+    // comments,
+  );
+});
+
+app.get("/register", (req, res) => {
+  console.log(req.body);
+  res.render("registration");
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  pool
+    .query(
+      `
+  INSERT INTO users (name,email,password)
+  VALUES($1,$2,$3)
+  RETURNING*;
+  `,
+      [name, email, password]
+    )
+    .then((result) => {
+      console.log(result.rows[0]);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
