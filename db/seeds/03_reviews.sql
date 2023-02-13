709429a9c18335b7cfd7ac195a424f2ec94515ac
RJ <randy.j416@yahoo.com>
2023-02-13 14:12:52 -0500

    add seeds pt 2

---
+++ db/seeds/03_reviews.sql
@@ -0,0 +1,5 @@
+INSERT INTO reviews (resource_id, user_id, comment) VALUES ('1', '1', 'Thanks for the resource!');
+INSERT INTO reviews (resource_id, user_id, comment) VALUES ('2', '1', 'Thanks for the resource!');
+INSERT INTO reviews (resource_id, user_id, comment) VALUES ('3', '1', 'Thanks for the resource!');
+INSERT INTO reviews (resource_id, user_id, comment) VALUES ('4', '2', 'Thanks for the site!');
+INSERT INTO reviews (resource_id, user_id, comment) VALUES ('1', '2', 'Thanks for the site!');
